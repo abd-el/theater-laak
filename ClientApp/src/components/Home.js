d798fd04211a5942709d81a357aafe95059854ba
dylan rampersad <67800304+dylanrsp@users.noreply.github.com>
2022-12-21 21:53:15 +0100

    Update Home.js
    Comment toegevoegd

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -4,7 +4,7 @@ import '../styling/Home.css';
 
 const theater = `https://media.istockphoto.com/photos/
 high-contrast-image-of-movie-theater-screen-picture-id487619252?k=6&m=487619252&s=612x612&w=0&h=
-yc_JV69FPuvIVNeukQpqZdcnIX7V-Vowj7Aih6sNue4=`;
+yc_JV69FPuvIVNeukQpqZdcnIX7V-Vowj7Aih6sNue4=`; //placeholder image
 
 export function Home() {
 
