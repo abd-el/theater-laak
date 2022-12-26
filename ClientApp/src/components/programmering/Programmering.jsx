865ef7bc45e0956dfb0c7fcc3ca928d641e72be7
yuriyvizor <101334510+yuriyvizor@users.noreply.github.com>
2022-12-26 18:40:51 +0100

    frontend programmering(niet functioneel)

--- ClientApp/src/components/programmering/Programmering.jsx
+++ ClientApp/src/components/programmering/Programmering.jsx
@@ -1,28 +1,30 @@
 import './Programmering.css';
 import { SearchBars } from './SearchBars';
 import { VoorstellingLijst } from './VoorstellingList';
+import '../../components/images/voorstellingVoorbeeld.jpg';
 
 export function Programmering() {
 
     const Voorstellingen = [
         {
             VoorstellingId: 1,
-            Titel: 'Kabouter en zijn vrienden',
-            Beschrijving: 'Kabouter piet gaat met zijn vrienden op tocht en er gebeuren een heleboel leuke dingen...',
+            Afbeelding: '../images/voorstellingVoorbeeld.jpg',
+            Titel: 'Voorbeeld 1',
+            Beschrijving: 'Some text...',
             TijdsduurInMinuten: 120
         },
 
         {
             VoorstellingId: 2,
-            Titel: 'De oude mans magie',
-            Beschrijving: 'Piet is een oude tovenaar...',
+            Titel: 'Voorbeeld 2',
+            Beschrijving: 'Some text...',
             TijdsduurInMinuten: 60
         },
 
         {
             VoorstellingId: 3,
-            Titel: 'De 3 slangen en de rode boom',
-            Beschrijving: '3 slangen die binnen de heilige rode boom proberen in te breken...',
+            Titel: 'Voorbeeld 3',
+            Beschrijving: 'Some text...',
             TijdsduurInMinuten: 240
         },
     ]
@@ -31,7 +33,57 @@ export function Programmering() {
             <br />
             <br />
             <br />
-           
+            <br />
+            <br />
+            <div className='inputs'>
+                <input id='searchbar' placeholder='zoek voorstelling' />
+                <input id='date' placeholder='kies een datum' type='date' />
+            </div>
+            <br />
+            <div className='buttons'>
+                <button id='day'>Dag</button>
+                <button id='week'>Week</button>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div>
+                <table className="table table-bordered table-striped table-dark">
+                    <thead>
+                        <tr>
+                            <th scope='col'>
+                                Afbeelding
+                            </th>
+                            <th scope='col'>
+                                Titel
+                            </th>
+                            <th scope='col'>
+                                Dag/Datum
+                            </th>
+                            <th scope='col'>
+                                Tijdstip
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {Voorstellingen.map((Voorstellingen) =>(
+                                    <tr>
+                                    <td className="afbeelding"><img src='../images/voorstellingVoorbeeld.jpg' alt='voorstellingsafbeelding'/>
+                                    </td>
+                                    <td className="titel">
+                                        {Voorstellingen.Titel}
+                                    </td>
+                                    <td className="dag-datum">
+                                        Vandaag
+                                    </td>
+                                    <td className="tijdstip">
+                                        15:30 - 17:15
+                                    </td>
+                                </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
