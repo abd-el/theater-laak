654c25512a1cfe1b81093b78c970b7f5dd62d111
dylan rampersad <67800304+dylanrsp@users.noreply.github.com>
2022-12-28 01:46:17 +0100

    Database update
    datebase geupdate met nieuwste migratie

---
+++ Data/Migrations/20221228004253_EmailVoorkeur.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace theater_laak.Data.Migrations
+{
+    public partial class EmailVoorkeur : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nieuwsbrief",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Emailvoorkeur",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IkDoneerToken",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefoonnummer",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Emailvoorkeur",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IkDoneerToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Telefoonnummer",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Nieuwsbrief",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
