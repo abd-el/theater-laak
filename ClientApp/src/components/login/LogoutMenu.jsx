b516048987b1801e01f6e4c2d79c8b26291a2505
dylan rampersad <67800304+dylanrsp@users.noreply.github.com>
2023-01-01 05:33:38 +0100

    Login en Logout menu toegevoegd
    LoginMenu component toegevoegd dat verschillende child components rendered (login of logout components )op basis van de user login state

---
+++ ClientApp/src/components/login/LogoutMenu.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useLogin } from "../hooks/useLogin";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { NavLink, NavItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
+
+export function LogoutMenu() {
+    const { logout } = useLogin();
+    const { authState } = useAuthContext();
+    let message;
+    authState == null ? (message = '') : (message = authState.user.userName)
+
+    return (
+        <>
+            <NavItem>
+                <NavLink tag={Link} className="text-light bg-dark" to=''>Hallo {message}</NavLink>
+            </NavItem>
+
+            <NavItem>
+                <NavLink tag={Link} onClick={logout} className="text-light bg-dark" to='' >Loguit</NavLink>
+            </NavItem>
+        </>
+    );
+}
