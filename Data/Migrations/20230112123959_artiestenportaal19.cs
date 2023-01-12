74b7ffc1e5d1da66ba83fd77c30db19a63beaf50
abd-el <101092777+abd-el@users.noreply.github.com>
2023-01-12 13:40:30 +0100

    cascade on delete

---
+++ Data/Migrations/20230112123959_artiestenportaal19.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace theater_laak.Data.Migrations
+{
+    public partial class artiestenportaal19 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Artiest_ArtiestenGroep_2",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Donatie_ApplicationUser_2",
+                table: "Donaties");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_ApplicationUser_2",
+                table: "Tickets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Artiest_ArtiestenGroep_2",
+                table: "AspNetUsers",
+                column: "ArtiestenGroepId",
+                principalTable: "ArtiestGroepen",
+                principalColumn: "ArtiestenGroepId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Donatie_ApplicationUser_2",
+                table: "Donaties",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_ApplicationUser_2",
+                table: "Tickets",
+                column: "UserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Artiest_ArtiestenGroep_2",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Donatie_ApplicationUser_2",
+                table: "Donaties");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_ApplicationUser_2",
+                table: "Tickets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Artiest_ArtiestenGroep_2",
+                table: "AspNetUsers",
+                column: "ArtiestenGroepId",
+                principalTable: "ArtiestGroepen",
+                principalColumn: "ArtiestenGroepId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Donatie_ApplicationUser_2",
+                table: "Donaties",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_ApplicationUser_2",
+                table: "Tickets",
+                column: "UserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
