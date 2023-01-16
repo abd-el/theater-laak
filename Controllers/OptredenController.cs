0b64472ba7a7817b04c10e403c6d32f0c2291d1c
abd-el <101092777+abd-el@users.noreply.github.com>
2023-01-16 23:11:51 +0100

    waarom werkt het niet

--- Controllers/OptredenController.cs
+++ Controllers/OptredenController.cs
@@ -7,6 +7,10 @@ using theater_laak.Models;
 
 namespace theater_laak.Controllers;
 
+public class OptredenJsonGegevens {
+    public string optredenId { get; set; }
+}
+
 [Route("api/[controller]")]
 [ApiController]
 public class OptredenController : ControllerBase {
@@ -20,18 +24,31 @@ public class OptredenController : ControllerBase {
     }
 
     [HttpGet]
-    [Route("GetOptreden/{voorstellingId}")]
-    public async Task<ActionResult> GetOptreden(int voorstellingId) {
+    [Route("GetOptreden")]
+    public async Task<ActionResult> GetOptreden([FromForm] OptredenJsonGegevens gegevens) {
+        // tryparse optredenId
+        int id;
+        var success = int.TryParse(gegevens.optredenId, out id);
+        if(!success) {
+            return StatusCode(400, new { 
+                bericht = $"optredenId = {gegevens.optredenId} is geen geldig getal",
+                success = false
+            });
+        }
+
         var optreden = await _context.Optredens
             .Include(o => o.Artiest)
             .Include(o => o.ArtiestenGroep)
             .Include(o => o.Voorstelling)
             .Include(o => o.Zaal)
-            .Where(o => o.VoorstellingId == voorstellingId)
+            .Where(o => o.OptredenId == id)
             .FirstOrDefaultAsync();
 
         if (optreden == null) {
-            return NotFound();
+            return StatusCode(400, new { 
+                bericht = $"Optreden met optredenId = {id} niet gevonden",
+                success = false
+            });
         }
 
         return Ok(optreden);
