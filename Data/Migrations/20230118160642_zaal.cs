93eeffffcc88a460a8ede4c84488364dfdcdae75
abd-el <101092777+abd-el@users.noreply.github.com>
2023-01-18 18:02:35 +0100

    gefixt

---
+++ Data/Migrations/20230118160642_zaal.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace theater_laak.Data.Migrations
+{
+    public partial class zaal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AantalStoelen",
+                table: "Zalen");
+
+            migrationBuilder.DropColumn(
+                name: "Grootte",
+                table: "Zalen");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AantalStoelen",
+                table: "Zalen",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Grootte",
+                table: "Zalen",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
--- Models/Zaal.cs
+++ Models/Zaal.cs
@@ -4,60 +4,18 @@ namespace theater_laak.Models;
 
 public class Zaal
 {
-    public int AantalStoelen {get; set;}
-    // onder hoe het hoort te zijn
-    // {
-    //     get
-    //     {
-    //         return Stoelen.Count();
-    //     }
-    //     private set
-    //     {
-            
-    //     }
-    // }
     public int ZaalId { get; set; }
-    private string? _Grootte;
-    public string? Grootte
-    {
-        get
-        {
-            return _Grootte;
-        }
-        private set
-        {
-            if (AantalStoelen == 30)
-            {
-                value = "Ruimte";
-            }
-            else if (AantalStoelen < 150 && AantalStoelen > 0)
-            {
-                value = "Klein";
-            }
-            else if (AantalStoelen > 150 && AantalStoelen > 0)
-            {
-                value = "Groot";
-            }
-            else
-            {
-                value = "Onbekend";
-            }
-
-            _Grootte = value;
-        }
-    }
     
     public List<Optreden> Optredens { get; set; }
 
-    public List<Stoel> Stoelen { get; set; }
+    public List<Stoel> Stoelen { get; set; } = new List<Stoel>();
 
-    public bool AddRij(int aantalStoelen, int rang, int rij)
-    {
+    public bool AddRij(int aantalStoelen, int rang, int rij) {
         if (rang < 1 || rang > 3) {
             return false;
         } else {
             for (int i = 0; i < aantalStoelen; i++) {
-                Stoelen.Add(new Stoel() { 
+                this.Stoelen.Add(new Stoel() { 
                     Rang = rang,
                     Rij = rij
                 });
@@ -66,13 +24,12 @@ public class Zaal
         }
     }
 
-    public bool AddRang(int aantalStoelen, int aantalRijen, int rang)
-    {
+    public bool AddRang(int aantalStoelen, int aantalRijen, int rang) {
         if (rang < 1 || rang > 3) {
             return false;
         } else {
             for (int i = 0; i < aantalRijen; i++) {
-                AddRij(aantalStoelen, rang, i);
+                AddRij(aantalStoelen / aantalRijen, rang, i);
             }
             return true;
         }
