a10a0d089e03834e5a3a70ef2172e0a01d27f112
dylan rampersad <67800304+dylanrsp@users.noreply.github.com>
2023-01-18 16:27:18 +0100

    loginController comments
    comments toegevoegd ter verduidelijking van de login controller

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -34,9 +34,15 @@ public class LoginController : ControllerBase
         _context = context;
     }
 
+    //Deze code is een methode voor het inloggen van gebruikers, waarbij een loginmodel-parameter genaamd credentials wordt verwacht.
+    //De methode controleert eerst of de gebruiker momenteel is geblokkeerd en of deze status is verlopen.
+    //Vervolgens controleert de code of het opgegeven wachtwoord correct is.
+    //Als het wachtwoord niet correct is, wordt de gebruiker geblokkeerd als hij minstens drie mislukte inlogpogingen heeft gehad.
+    //Als het wachtwoord correct is, wordt de FailedAttempts-eigenschap van de gebruiker op null gezet.
+    //Als de TwoFactorEnabled-eigenschap van de gebruiker op true is ingesteld, wordt een e-mail verzonden met een 2FA-token.
+    //Als alles succesvol is afgerond wordt er een Swt-Token gegenereerd en teruggestuurd als antwoord.
 
     [HttpPost]
-    // [Route("login")]
     public async Task<ActionResult> login([FromBody] loginmodel credentials)
     {
 
@@ -98,31 +104,18 @@ public class LoginController : ControllerBase
 
     }
 
-    private async Task<string> Create2FaToken(ApplicationUser user)
-    {
-        var pwd = new Password(12);
-        var randomstring = pwd.Next();
-        var date = DateTime.Now;
-        var expire = date.AddMinutes(30);
-        user._2faExpDate = expire;
-        user._2faToken = randomstring;
-        await _usermanager.UpdateAsync(user);
 
-        return randomstring;
-    }
 
-    public class _2faJson { public string token { get; set; } public string userName { get; set; } }
+    public class TweeFactorAuthJsonObject { public string token { get; set; } public string userName { get; set; } }
     [HttpPost]
     [Route("isTokenValid")]
-    public async Task<ActionResult> isTokenValid(_2faJson json)
+    public async Task<ActionResult> isTokenValid(TweeFactorAuthJsonObject json)
     {
         var user = await _usermanager.FindByNameAsync(json.userName);
         if (json.token == user._2faToken)
         {
             if (DateTime.Compare(DateTime.Now, (DateTime)user._2faExpDate) <= 0)
             {
-                // user.EmailConfirmed = true;
-                // await _usermanager.UpdateAsync(user);
                 var tokenOptions = await GenerateJwt(user);
                 return Ok(new { Token = new JwtSecurityTokenHandler().WriteToken(tokenOptions), user });
             }
@@ -131,10 +124,13 @@ public class LoginController : ControllerBase
         return BadRequest("wrong");
     }
 
-    public class usernameJson { public string userName { get; set; } }
+
+
+
+    public class usernameJsonObject { public string userName { get; set; } }
     [HttpPost]
     [Route("mailToConfirmedAddress")]
-    public async Task<ActionResult> MailToConfirmedAddress([FromBody] usernameJson json)
+    public async Task<ActionResult> MailToConfirmedAddress([FromBody] usernameJsonObject json)
     {
         var user = await _usermanager.FindByNameAsync(json.userName);
         if (user == null)
@@ -149,9 +145,12 @@ public class LoginController : ControllerBase
         return Ok();
     }
 
+
+
+
     [HttpPost]
     [Route("mailToUnconfirmedAddress")]
-    public async Task<ActionResult> MailToUnconfirmedAddress([FromBody] usernameJson json)
+    public async Task<ActionResult> MailToUnconfirmedAddress([FromBody] usernameJsonObject json)
     {
         var user = await _usermanager.FindByNameAsync(json.userName);
         if (user == null)
@@ -162,37 +161,9 @@ public class LoginController : ControllerBase
         return Ok();
     }
 
-    private async Task SendTokenToEmail(ApplicationUser user)
-    {
-        var token = await Create2FaToken(user);
-        //await emailsender.SendEmail($"Uw toegangscode is: {token} De code verloopt na 30 minuten", "drampersad740@gmail.com");
-        await emailsender.SendEmail($"Uw toegangscode is: {token} De code verloopt na 30 minuten", user.Email);
-    }
 
 
-    private async Task<JwtSecurityToken> GenerateJwt(ApplicationUser user)
-    {
-        var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("awef98awef978haweof8g7aw789efhh789awef8h9awh89efh89awe98f89uawef9j8aw89hefawef"));
-
-        var signingCredentials = new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
-        var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.UserName) };
-        var roles = await _usermanager.GetRolesAsync(user);
-
-        foreach (var role in roles)
-            claims.Add(new Claim(ClaimTypes.Role, role));
-        var tokenOptions = new JwtSecurityToken
-        (
-            issuer: "https://localhost:7209",
-            audience: "https://localhost:7209",
-            claims: claims,
-            expires: DateTime.Now.AddMinutes(30),
-            signingCredentials: signingCredentials
-        );
-
-        return tokenOptions;
-    }
-
-    public class captchaResponse { public string responseToken { get; set; } }
+    public class captchaResponseJsonObject { public string responseToken { get; set; } }
     public class ApiResponse
     {
         public bool success { get; set; }
@@ -200,9 +171,12 @@ public class LoginController : ControllerBase
         public string hostname { get; set; }
     }
 
+
+
+
     [HttpPost]
     [Route("ReCaptcha")]
-    public async Task<ActionResult> test([FromBody] captchaResponse _captchaResponse)
+    public async Task<ActionResult> test([FromBody] captchaResponseJsonObject _captchaResponse)
     {
         bool IsHuman = false;
 
@@ -233,6 +207,11 @@ public class LoginController : ControllerBase
         return Ok(IsHuman);
     }
 
+
+    //Deze methode controleert of een SWT Token niet verlopen is. En of een kwaardaardige persoon de token niet heeft aangepast.
+    //De methode is beschikbaar onder de route "validateSwtToken" en is beveiligd met de Authorize-attribuut,
+    //dit betekent dat alleen gebruikers met een geldige swt token kunnen toegang krijgen tot deze route.
+    //De methode retourneert een "Ok" HTTP-statuscode, dit betekent dat de aanvraag succesvol is afgerond en dat er geen informatie teruggegeven hoeft te worden.
     [HttpGet]
     [Route("validateSwtToken")]
     [Authorize]
@@ -240,5 +219,53 @@ public class LoginController : ControllerBase
     {
 
         return Ok();
+    }
+
+
+
+    private async Task SendTokenToEmail(ApplicationUser user)
+    {
+        var token = await Create2FaToken(user);
+        await emailsender.SendEmail($"Uw toegangscode is: {token} De code verloopt na 30 minuten", "drampersad740@gmail.com");
+        //await emailsender.SendEmail($"Uw toegangscode is: {token} De code verloopt na 30 minuten", user.Email);
+    }
+
+
+    private async Task<string> Create2FaToken(ApplicationUser user)
+    {
+        var pwd = new Password(12);
+        var randomstring = pwd.Next();
+        var date = DateTime.Now;
+        var expire = date.AddMinutes(30);
+        user._2faExpDate = expire;
+        user._2faToken = randomstring;
+        await _usermanager.UpdateAsync(user);
+
+        return randomstring;
+    }
+
+    //De methode genereerd SWT-Token Options en stuurt deze terug. Zodat de methode de swt-token kan signen met deze options.
+    //De signature is het resultaat van het hashen van de header en payload met een geheime sleutel.
+    //Dit zorgt ervoor dat alleen partijen met de juiste sleutel het token kunnen verifiëren en de claims kunnen lezen.
+    private async Task<JwtSecurityToken> GenerateJwt(ApplicationUser user)
+    {
+        var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("awef98awef978haweof8g7aw789efhh789awef8h9awh89efh89awe98f89uawef9j8aw89hefawef"));
+
+        var signingCredentials = new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.UserName) };
+        var roles = await _usermanager.GetRolesAsync(user);
+
+        foreach (var role in roles)
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        var tokenOptions = new JwtSecurityToken
+        (
+            issuer: "https://localhost:7209",
+            audience: "https://localhost:7209",
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: signingCredentials
+        );
+
+        return tokenOptions;
     }
 }
