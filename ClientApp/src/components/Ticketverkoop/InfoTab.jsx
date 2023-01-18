b7274c57a25fa680d36925ed8d5c4752d5834146
yuriyvizor <101334510+yuriyvizor@users.noreply.github.com>
2023-01-18 21:08:22 +0100

    useref

--- ClientApp/src/components/Ticketverkoop/InfoTab.jsx
+++ ClientApp/src/components/Ticketverkoop/InfoTab.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 export function InfoTab(props) {
 
     const [error, setError] = useState(null);
