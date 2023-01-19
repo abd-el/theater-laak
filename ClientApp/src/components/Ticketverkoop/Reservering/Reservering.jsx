e018151d03a82a8722a9e00d44a3ae580575819d
dylan rampersad <67800304+dylanrsp@users.noreply.github.com>
2023-01-19 03:28:10 +0100

    minor bugfix

--- ClientApp/src/components/Ticketverkoop/Reservering/Reservering.jsx
+++ ClientApp/src/components/Ticketverkoop/Reservering/Reservering.jsx
@@ -11,15 +11,15 @@ export function Reservering() {
             <Container fluid={true} className='col-md-10'>
                 <Figure />
 
-                <Card body className="text-start mb-3 bg-dark h4">
+                <Card body className="crd text-start mb-3 bg-dark h4">
                     <CardTitle>1e rang</CardTitle>
                     <CardText>50€ per stoel</CardText>
                 </Card>
-                <Card body className="text-start mb-3 bg-dark h4">
+                <Card body className="crd text-start mb-3 bg-dark h4">
                     <CardTitle>2e rang</CardTitle>
                     <CardText>25€ per stoel</CardText>
                 </Card>
-                <Card body className="text-start mb-3 bg-dark h4">
+                <Card body className="crd text-start mb-3 bg-dark h4">
                     <CardTitle>3e rang</CardTitle>
                     <CardText>10€ per stoel</CardText>
                 </Card>
