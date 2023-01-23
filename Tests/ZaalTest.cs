9c8fab64b2c609b6e41c8139dccff6abbee22bed
abd-el <101092777+abd-el@users.noreply.github.com>
2023-01-23 17:10:21 +0100

    Zaal fix + tests

---
+++ Tests/ZaalTest.cs
@@ -0,0 +1,45 @@
+namespace ZaalTest;
+
+using theater_laak.Models;
+using Xunit;
+
+public class ZaalTest
+{
+    [Fact]
+    public void AddRijTest()
+    {
+        // Arrange
+        var zaal = new Zaal();
+        var aantalStoelen = 10;
+        var rang = 1;
+        var rij = 1;
+
+        var expectedAantalStoelen = 10;
+        
+        // Act
+        zaal.AddRij(aantalStoelen, rang, rij);
+        var actualAantalStoelen = zaal.Stoelen.Count;
+        // Assert
+
+        Assert.Equal(expectedAantalStoelen, actualAantalStoelen);
+    }
+
+    [Fact]
+    public void AddRangTest()
+    {
+        // Arrange
+        var zaal = new Zaal();
+        var aantalStoelen = 10;
+        var aantalRijen = 2;
+        var rang = 1;
+
+        var expectedAantalStoelen = 10;
+        
+        // Act
+        zaal.AddRang(aantalStoelen, aantalRijen, rang);
+        var actualAantalStoelen = zaal.Stoelen.Count;
+
+        // Assert
+        Assert.Equal(expectedAantalStoelen, actualAantalStoelen);
+    }
+}
